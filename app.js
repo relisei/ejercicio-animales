c893c582072ea7694372f688f23395202f2e76b9
ramiro elisei <ramiroelisei@gmail.com>
2023-09-22 12:11:23 +0200

    Add animals objects

---
+++ app.js
@@ -0,0 +1,14 @@
+const animalA = {
+  name: "foca",
+  age: 23,
+  weight: 250,
+  species: "mamífero",
+};
+
+const animalB = {
+  name: "rinoceronte blanco",
+  age: 45,
+  weight: 500,
+  species: "mamifero",
+  extinction: 2018,
+};
