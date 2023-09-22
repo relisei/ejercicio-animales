a72cdce1ca945f5147f8858cfcb5e8d2d253fb17
ramiro elisei <ramiroelisei@gmail.com>
2023-09-22 12:20:16 +0200

    Add getSpecies

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const animalA = {
+  id: 1,
   name: "foca",
   age: 23,
   weight: 250,
@@ -6,9 +7,16 @@ const animalA = {
 };
 
 const animalB = {
+  id: 2,
   name: "rinoceronte blanco",
   age: 45,
   weight: 500,
   species: "mamifero",
   extinction: 2018,
 };
+
+const getEspecies = (animal) => {
+  return animal.species;
+};
+
+getEspecies(animalA);
